9b3e8e6aa740114c64a2c5cbaf6347c3b47b758f
Alina <linalevel5@gmail.com>
2022-11-22 11:39:31 +0300

    Initial Commit

---
+++ ListOfPeopleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListOfPeopleApp
+//
+//  Created by MAcbook on 22.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
